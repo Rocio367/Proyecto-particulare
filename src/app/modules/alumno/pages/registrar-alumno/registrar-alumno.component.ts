07d62be708539ef88b61cca3dc7bdd89b1329e47
Rocio Isaurralde <rocio.isau@gmail.com>
2021-09-27 17:29:09 -0300

    Campos basicos para registro de alumno

--- src/app/modules/alumno/pages/registrar-alumno/registrar-alumno.component.ts
+++ src/app/modules/alumno/pages/registrar-alumno/registrar-alumno.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-registrar-alumno',
   templateUrl: './registrar-alumno.component.html',
   styleUrls: ['./registrar-alumno.component.scss']
 })
-export class RegistrarAlumnoComponent implements OnInit {
+export class RegistrarAlumnoComponent {
+  formDatos: FormGroup;
+  fotoPerfilFormControl: FormControl;
+  tiposDeArchivosPermitidos = ".png, .jpg, .jpeg";
+  foto: any;
 
-  constructor() { }
+  constructor(private form: FormBuilder) {
 
-  ngOnInit(): void {
+    this.fotoPerfilFormControl = new FormControl([this.foto, Validators.required])
+
+    this.formDatos = this.form.group({
+      nombre: ['', Validators.required],
+      apellido: ['', Validators.required],
+      email: ['', [Validators.email, Validators.required]],
+      contrasenia: ['', Validators.required],
+      repetirContrasenia: ['', Validators.required],
+      fechaNacimiento: ['', Validators.required],
+      intereses: ['', Validators.required], 
+      institucuion: ['', Validators.required],
+      nivelAcademico: ['', Validators.required],
+      fotoPerfil: this.fotoPerfilFormControl
+    });
   }
 
 }
