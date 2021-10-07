1bc090ff323d99d192259462635b1be65a5db345
ldinubila <lucas.dinubila@gmail.com>
2021-10-06 22:43:11 -0300

    Pantalla contacto

---
+++ src/app/modules/contacto/contacto-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { ContactoComponent } from './pages/contacto.component';
+
+const routes:Routes = [
+  {path:'contacto', component:ContactoComponent},
+];
+
+@NgModule({
+  imports:[RouterModule.forChild(routes)],
+  exports:[RouterModule]
+})
+export class ContactoRoutingModule { }
