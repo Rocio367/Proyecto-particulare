40d7d426cfbf9679e3a3b396e348fc39f5da6700
Rocio Isaurralde <rocio.isau@gmail.com>
2021-10-09 22:37:11 -0300

    Prueba de algunos componentes

--- src/app/modules/demo-primeng/demoprimeng.module.ts
+++ src/app/modules/demo-primeng/demoprimeng.module.ts
@@ -2,11 +2,12 @@ import { CommonModule } from "@angular/common";
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { RouterModule } from "@angular/router";
 import { ButtonModule } from 'primeng/button';
+import {TooltipModule} from 'primeng/tooltip';
 import { DemoPrimeNgComponent } from "./components/demoprimeng.component";
 
 @NgModule({
     declarations:[DemoPrimeNgComponent],
-    imports:[ CommonModule, RouterModule,ButtonModule,],
+    imports:[ CommonModule, RouterModule,ButtonModule,TooltipModule,],
     exports:[],
     schemas:[ CUSTOM_ELEMENTS_SCHEMA ],
   })
