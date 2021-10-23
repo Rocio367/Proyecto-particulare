90c2f0aedb33003b55b3a848f26c8a6e9029b3c3
Rocio Isaurralde <rocio.isau@gmail.com>
2021-10-23 17:57:42 -0300

    Servicio para obtener materias y niveles

--- src/app/core/services/modelos/modelos.service.ts
+++ src/app/core/services/modelos/modelos.service.ts
@@ -2,7 +2,9 @@ import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Documento } from 'src/app/shared/models/documento';
+import { Materia } from 'src/app/shared/models/materia';
 import { Modelo } from 'src/app/shared/models/modelo';
+import { Nivel } from 'src/app/shared/models/nivel';
 import { environment } from "../../../../environments/environment";
 
 @Injectable({
@@ -21,5 +23,14 @@ export class ModelosService {
 
   obtenerArchivosPorModelo(modelo: Modelo): Observable<Documento[]>{
     return this.http.get<Documento[]>(`${environment.backUrl}/v1/modelos/${modelo.id}/archivos`);
+  }
+
+  // ¿Lo movemos a MateriasService y NivelesServices?
+  obtenerMaterias(): Observable<Materia[]> {
+    return this.http.get<Materia[]>(`${environment.backUrl}/v1/materias`);
+  }
+
+  obtenerNiveles(): Observable<Nivel[]> {
+    return this.http.get<Nivel[]>(`${environment.backUrl}/v1/niveles`);
   }
 }
