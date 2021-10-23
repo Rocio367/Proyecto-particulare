2c1c8202102b04885ef28986981422141a87f4f5
Rocio Isaurralde <rocio.isau@gmail.com>
2021-10-23 11:00:48 -0300

    Obtengo los archivos asociados a un modelo

--- src/app/core/services/modelos/modelos.service.ts
+++ src/app/core/services/modelos/modelos.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Documento } from 'src/app/shared/models/documento';
 import { Modelo } from 'src/app/shared/models/modelo';
 import { environment } from "../../../../environments/environment";
 
@@ -12,5 +13,13 @@ export class ModelosService {
 
   subirModelo(modelo: Modelo): Observable<void> {
     return this.http.post<void>(`${environment.backUrl}/v1/modelos`, modelo);
+  }
+
+  obtenerModelos(): Observable<Modelo[]> {
+    return this.http.get<Modelo[]>(`${environment.backUrl}/v1/modelos`);
+  }
+
+  obtenerArchivosPorModelo(modelo: Modelo): Observable<Documento[]>{
+    return this.http.get<Documento[]>(`${environment.backUrl}/v1/modelos/${modelo.id}/archivos`);
   }
 }
