4d87e9884474ef958b9f0d3ab65e6284bae35cc9
ldinubila <lucas.dinubila@gmail.com>
2021-10-31 02:37:12 -0300

    Creacion servicio y modelo de usuario

---
+++ src/app/core/services/usuarios/usuarios.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { environment } from "../../../../environments/environment";
+import { Usuario } from "src/app/shared/models/usuario";
+
+@Injectable({
+    providedIn: "root",
+  })
+  export class ClaseService {
+    constructor(private http: HttpClient) {}
+
+
+    registrarAlumno(usuario: Usuario): Observable<void> {
+        return this.http.post<void>(`${environment.backUrl}/v1/usuarios`, usuario);
+      }
+
+
+}
---
+++ src/app/shared/models/usuario.ts
@@ -0,0 +1,9 @@
+export interface Usuario{
+    nombre: string;
+    apellido: string;
+    documento: number;
+    email: string;
+    contrasenia: string;
+    telefono: number;
+    fechaNacimiento: Date;
+}
