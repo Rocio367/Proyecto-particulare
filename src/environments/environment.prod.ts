d5a1c1b0ba5db03f18e0c22d6a47e49cc3fb959f
Rocio Isaurralde <rocio.isau@gmail.com>
2021-11-21 16:32:54 -0300

    Agrego url del back en azure

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,4 +2,5 @@ export const environment = {
   production:true,
   apiUrl:'http://tgnoverhaul.smartworkar.com:8888',
   frontUrl:'http://localhost:4200',
+  backUrl: 'https://particulares.api.azurewebsites.net/particulares/api',
 };
