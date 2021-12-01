e00f51b8b61158c94712db3aae6b49858dacc3ef
fioreColoca <fiorecoloca99@gmail.com>
2021-11-30 22:22:39 -0300

    ahora se puede ver la imagen en otros navegadores

--- src/app/modules/particular/pages/mi-perfil-particular/mi-perfil-particular.component.ts
+++ src/app/modules/particular/pages/mi-perfil-particular/mi-perfil-particular.component.ts
@@ -193,7 +193,8 @@ export class MiPerfilParticularComponent implements OnInit {
 
   oponDoc(doc){
     this.info = this.obtenerImagenEnBase64(doc);
-    window.open(this.info)
+    var win = window.open();
+    win.document.write('<iframe src="' + this.info  + '" frameborder="0" style="border:0; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%;" allowfullscreen></iframe>');
   }
 
   onUpload(event) {
